1cab3031ebaff4da113510cdfb7fc4b7122e4b17
Bailey Spraggins <baileyspraggins@Baileys-MBP.attlocal.net>
2022-05-08 21:37:06 -0500

    Completed rust program

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # gm-program
+
+## Description
+
+- Solana program created during the Chainlink Solana Bootcamp to get comfortable interacting with the Solana Blockchain
+
+## Features
+
+- A Simple CLI client that displays the account, program and client account as well as a message saying hello and greeting the user with Good Morning.
+
+## Concepts Used
+
+- Solana Programs and Accounts
+- Serialization and Deserialization
+- Solana Web3.JS RPC
+
+## How to interact with this code.
+
+- By default this code is run on a local cluster. If you wish you change to the Solana devnet make sure your cli cluster is set as well as line 28 in utils.js is set to "https://api.devnet.solana.com"
+- To se the name of who you would like to say good morning to please enter a string for the name parameter in line 63 in the gm_program.ts file.
+
+## Running the Program
+
+- In the root of the project director run "cargo build-bpf" to build the project.
+- Follow the prompt to deploy to your local cluster
+- After your program is deployed enter the command npm run start and the program will execute
+
+## Future Additions to project
+
+- Store numbers in the account as well as how many times the account has said GM to it.
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,50 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+// Define type of state stored in account
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct GreetingAccount {
+    pub name: String,
+}
+
+// Declare abnd export eh program's entrypoint
+entrypoint!(process_instruction);
+
+// Programs entrypoints implementation
+pub fn process_instruction(
+    program_id: &Pubkey,      // Public key of the account
+    accounts: &[AccountInfo], // Account that the program is interacting with
+    input: &[u8],             // String input data that the program will say Good Morning too
+) -> ProgramResult {
+    msg!("GM program entrypoint");
+
+    // Iterate through accounts
+    let accounts_iter = &mut accounts.iter();
+
+    // Get the account we will be interacting with
+    let account = next_account_info(accounts_iter)?;
+
+    // Make sure that the account is owned by the program
+    if account.owner != program_id {
+        msg!("Greeted account does not ahve the correct program id");
+        return Err(ProgramError::IncorrectProgramId);
     }
+
+    // Deserialzie the input data and store in the greeting account struct
+    let input_data = GreetingAccount::try_from_slice(&input).unwrap();
+
+    // Say GM in the program output
+    msg!("Good Morning! {}", input_data.name);
+
+    // Serialize the name and store it in the passed account
+    input_data.serialize(&mut &mut account.try_borrow_mut_data()?[..])?;
+
+    Ok(())
 }
